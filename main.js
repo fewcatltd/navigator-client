d6e7608d1d510236c34d3ff7233be25d07a2069f
fewcatltd <dokotov@gmail.com>
2023-11-30 17:35:07 +0400

    change forge-makers

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const {app, BrowserWindow} = require('electron');
 const path = require("path");
 const packageJson = require('./package.json');
 const {updateElectronApp} = require('update-electron-app')
-updateElectronApp()
 
 let mainWindow;
 
@@ -34,7 +33,10 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+    updateElectronApp()
+    return createWindow()
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
