d15f019d97ab07785b609677c4c056ee29583fa8
sashakaralchuk <sasha.karalchuk@gmail.com>
2024-06-18 18:09:03 +0200

    feature: add prod debug opportunity

--- app.mjs
+++ app.mjs
@@ -1,23 +1,35 @@
 import io from 'https://cdn.jsdelivr.net/npm/socket.io-client@4.0.1/+esm'
-import { useAuth } from './auth.mjs'
+import * as viem from 'https://cdn.jsdelivr.net/npm/viem@1.15.4/+esm'
+
+const {ipcRenderer} = require('electron')
+
+import {useAuth} from './auth.mjs'
 import * as utils from './utils.mjs'
-import { useZora } from './zora.mjs'
-import {useZkSync} from "./zksync.mjs";
-import {useScroll} from "./scroll.mjs";
-import {useLinea} from "./linea.mjs";
-import {useBase} from "./base.mjs";
-import {useHyperlane} from "./hyperlane.mjs";
-const { ipcRenderer } = require('electron');
-const packageJson = require('./package.json');
+import {useZora} from './zora.mjs'
+import {useZkSync} from './zksync.mjs'
+import {useScroll} from './scroll.mjs'
+import {useLinea} from './linea.mjs'
+import {useBase} from './base.mjs'
+import {useHyperlane} from './hyperlane.mjs'
 
-const { createApp, ref } = window.Vue
+const cosmJsEncoding = require('@cosmjs/encoding')
+const packageJson = require('./package.json')
 
-// const SOCKET_SERVER_URL = `http://localhost:3000/?version=` + packageJson.version
-const SOCKET_SERVER_URL = `https://fewcats.com/?version=` + packageJson.version
+const {createApp, ref} = window.Vue
+
+const SOCKET_SERVER_URL_DEFAULT =
+  `https://fewcats.com/?version=` + packageJson.version
+const SOCKET_SERVER_URL =
+  process.env.SOCKET_SERVER_URL || SOCKET_SERVER_URL_DEFAULT
+
+window.debug = {
+  vars: {SOCKET_SERVER_URL, cosmJsEncoding, viem},
+  envs: {SOCKET_SERVER_URL: process.env.SOCKET_SERVER_URL},
+}
 
 const isConnected = ref(false)
-const isUpdateAvailable = ref(false);
-const isUpdateDownloaded = ref(false);
+const isUpdateAvailable = ref(false)
+const isUpdateDownloaded = ref(false)
 
 const socket = io(SOCKET_SERVER_URL)
 socket.on('connect', () => {
@@ -28,29 +40,32 @@ socket.on('disconnect', () => {
 })
 socket.on('error', () => {
   console.log('Error from server', 'SocketID:', socket.id)
-  socket.disconnect();
+  socket.disconnect()
   setTimeout(() => {
-    socket.connect();
-  }, 5000);
+    socket.connect()
+  }, 5000)
 })
 
 ipcRenderer.on('update_available', () => {
-  isUpdateAvailable.value = true;
-});
+  isUpdateAvailable.value = true
+})
 
 ipcRenderer.on('update_downloaded', () => {
-  isUpdateDownloaded.value = true;
-});
+  isUpdateDownloaded.value = true
+})
 
 ipcRenderer.on('version', (event, version) => {
-  window.VERSION = version;
-  document.querySelector('.Version').innerHTML = 'Version: ' + version;
-});
-let workspaceInstance = null;
+  window.VERSION = version
+  document.querySelector('.Version').innerHTML = 'Version: ' + version
+})
+let workspaceInstance = null
 const WorkspaceUI = {
-  async setup () {
-    if(workspaceInstance) return workspaceInstance;
-    const activeBlockchainTab = utils.useLocalStorageRef('active-blockchain-tab', 'zora')
+  async setup() {
+    if (workspaceInstance) return workspaceInstance
+    const activeBlockchainTab = utils.useLocalStorageRef(
+      'active-blockchain-tab',
+      'zora',
+    )
     const zora = await useZora(socket)
     const zkSync = await useZkSync(socket)
     const scroll = await useScroll(socket)
@@ -67,12 +82,11 @@ const WorkspaceUI = {
       activeBlockchainTab,
     }
 
-    return workspaceInstance;
+    return workspaceInstance
   },
   template: `
     <div class="WorkspaceView" style="margin-top: 50px;">
       <h2>Добро пожаловать в Airdrop Navigator!</h2>
-
       <el-tabs v-model="activeBlockchainTab">
         <el-tab-pane label="Zora" name="zora">
           <component :is="ZoraUI"/>
@@ -98,9 +112,8 @@ const WorkspaceUI = {
 }
 
 const app = createApp({
-  setup () {
+  setup() {
     const auth = useAuth(socket)
-
     return {
       AuthUI: auth.UI,
       WorkspaceUI,
@@ -111,13 +124,13 @@ const app = createApp({
   data() {
     return {
       isUpdateAvailable,
-      isUpdateDownloaded
-    };
+      isUpdateDownloaded,
+    }
   },
   methods: {
     restartApp() {
-      ipcRenderer.send('restart_app');
-    }
+      ipcRenderer.send('restart_app')
+    },
   },
   template: `
     <div>
@@ -137,21 +150,62 @@ const app = createApp({
         Перезагрузить
       </el-button>
     </div>
-
       <div v-if="!isAuthorized" class="AuthView">
         <component :is="AuthUI"/>
       </div>
-
       <div v-else-if="!isConnected" class="ConnectingView">
         <h2 class="PageTitle">Подключение к серверу...</h2>
       </div>
-      
       <suspense v-else>
         <component :is="WorkspaceUI"/>
       </suspense>
     </div>
   `,
 })
+
 app.use(window.ElementPlus)
-Object.entries(window.ElementPlusIconsVue).map(([key, value]) => app.component(key, value))
+Object.entries(window.ElementPlusIconsVue).map(([key, value]) =>
+  app.component(key, value),
+)
 app.mount('#app')
+
+class ListenDoubleClicks {
+  #pressedKeys
+  #callbacks
+
+  constructor(callbacks) {
+    this.#pressedKeys = {}
+    this.#callbacks = callbacks
+  }
+
+  initialize = () => {
+    document.addEventListener('keydown', this.#handleKeyDown)
+  }
+
+  #handleKeyDown = event => {
+    const code = event.code
+    const now = Date.now()
+    if (!(code in this.#pressedKeys)) {
+      this.#pressedKeys[code] = now
+      return
+    }
+    const ts = this.#pressedKeys[code]
+    if (now - ts > 1000) {
+      this.#pressedKeys[code] = now
+      return
+    }
+    delete this.#pressedKeys[code]
+    const key = `${code}-${code}`
+    if (!(key in this.#callbacks)) {
+      return
+    }
+    this.#callbacks[key]()
+  }
+}
+
+window.addEventListener('load', () => {
+  const handleKK = () => ipcRenderer.send('open-dev-tools')
+  // eslint-disable-next-line no-console
+  console.debug('added k+k listener')
+  new ListenDoubleClicks({'KeyK-KeyK': handleKK}).initialize()
+})
--- main.js
+++ main.js
@@ -1,61 +1,58 @@
-const {app, BrowserWindow} = require('electron');
-const path = require("path");
-const packageJson = require('./package.json');
+const path = require('path')
+
+const {app, BrowserWindow, ipcMain} = require('electron')
 const {updateElectronApp} = require('update-electron-app')
 
-let mainWindow;
+const packageJson = require('./package.json')
+
+let mainWindow
 
 if (require('electron-squirrel-startup')) {
-    app.quit()
+  app.quit()
 }
 
 function createWindow() {
-    if (require('electron-squirrel-startup')) return
-
-    mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
-        icon: path.join(__dirname, 'resources/icon.png'),
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
-
-    mainWindow.loadFile('index.html');
-    mainWindow.webContents.on('did-finish-load', () => {
-        mainWindow.webContents.send('version', packageJson.version);
-    });
-
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
-
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools();
+  if (require('electron-squirrel-startup')) {
+    return
+  }
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    icon: path.join(__dirname, 'resources/icon.png'),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+  mainWindow.loadFile('index.html')
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.webContents.send('version', packageJson.version)
+  })
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-    updateElectronApp()
-    return createWindow()
-});
+  updateElectronApp()
+  return createWindow()
+})
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+ipcMain.on('open-dev-tools', () => {
+  if (!mainWindow) {
+    return
+  }
+  mainWindow.webContents.openDevTools()
+})
